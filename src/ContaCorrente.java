16d0662340ef067d847f8307a331ce2813fdab37
Assumps <jrvitor5@gmail.com>
2021-10-14 06:30:58 -0300

    Entendendo os conceitos de Heranca e Interfaces e quando usa-las

--- src/ContaCorrente.java
+++ src/ContaCorrente.java
@@ -1,12 +1,22 @@
-public class ContaCorrente  extends Conta{
+public class ContaCorrente  extends Conta implements Tributavel{
     public ContaCorrente (int agencia, int numero){
         super(agencia,numero);
 
     }
 
     @Override
+    public void deposita(double valor) {
+        super.saldo = super.saldo + valor;
+    }
+
+    @Override
     public boolean saca(double valor) {
         double valoraSacar = valor + 0.2;
         return super.saca(valoraSacar);
+    }
+
+    @Override
+    public double getValorImposto() {
+        return 42;
     }
 }
